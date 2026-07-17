5133296791622711f2d6ea12c269249af6372882
Saransh Dhiman <saranshdhiman353@gmail.com>
2026-07-17 18:32:07 +0530

    Adding the logs for perfect flow and saving the ai genraetd response in db

--- ai-service/src/main/java/com/microservice/project/ai_service/service/ActivityMessageListener.java
+++ ai-service/src/main/java/com/microservice/project/ai_service/service/ActivityMessageListener.java
@@ -1,6 +1,8 @@
 package com.microservice.project.ai_service.service;
 
 import com.microservice.project.ai_service.entity.Activity;
+import com.microservice.project.ai_service.entity.Recommendation;
+import com.microservice.project.ai_service.repository.RecommendationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -12,13 +14,16 @@ import org.springframework.stereotype.Service;
 public class ActivityMessageListener {
 
     private final ActivityAIService aiService;
+    private final RecommendationRepository  recommendationRepository;
 
     @RabbitListener(queues = "${rabbitmq.queue.name}")
     public void processActivity(Activity activity) {
         try {
             log.info("Received activity message: {}", activity.getId());
-            String recommendation = aiService.generateRecommendation(activity);
-            log.info("Generated Recommendation: {}", recommendation);
+            Recommendation recommendation = aiService.generateRecommendation(activity);
+            log.info("AI RESPONSE GENERATED SUCCESSFULLY ");
+            recommendationRepository.save(recommendation);
+            log.info("AI RESPONSE SAVED IN DB SUCCESSFULLY");
         } catch (Exception e) {
             log.error("Failed to process activity {}: {}", activity.getId(), e.getMessage());
             // Don't re-throw - this prevents infinite redelivery
