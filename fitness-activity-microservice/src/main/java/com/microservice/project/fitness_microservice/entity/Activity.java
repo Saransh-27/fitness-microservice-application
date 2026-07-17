b97d73d9a969279e99128092fa04a4c613572760
Saransh Dhiman <saranshdhiman353@gmail.com>
2026-07-17 18:36:37 +0530

    Adding the collection name in which the data is being stored

--- fitness-activity-microservice/src/main/java/com/microservice/project/fitness_microservice/entity/Activity.java
+++ fitness-activity-microservice/src/main/java/com/microservice/project/fitness_microservice/entity/Activity.java
@@ -1,11 +1,13 @@
 package com.microservice.project.fitness_microservice.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.time.LocalDateTime;
@@ -14,6 +16,8 @@ import java.util.Map;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
+@Document(collection = "activities")
 public class Activity {
     @Id
     private String id;
