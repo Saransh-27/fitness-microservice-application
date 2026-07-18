a96de88e88b6c0abf8e5dc00be3ac248a47b891e
Saransh Dhiman <saranshdhiman353@gmail.com>
2026-07-18 20:06:37 +0530

    Adding the prefix for the apis

--- fitness-activity-microservice/src/main/java/com/microservice/project/fitness_microservice/contoller/ActivityController.java
+++ fitness-activity-microservice/src/main/java/com/microservice/project/fitness_microservice/contoller/ActivityController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/activities")
+@RequestMapping("/apis/activities")
 public class ActivityController {
 
     @Autowired
--- user-microservice/src/main/java/com/microservice/project/fitness/controller/UserController.java
+++ user-microservice/src/main/java/com/microservice/project/fitness/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/apis/users")
 @Slf4j
 public class UserController {
 
