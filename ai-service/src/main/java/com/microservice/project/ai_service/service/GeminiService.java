06ebea5d0dae617c51345c49d2608ded1a08c910
Saransh Dhiman <saranshdhiman353@gmail.com>
2026-07-25 19:42:48 +0530

    fixing

--- ai-service/src/main/java/com/microservice/project/ai_service/service/GeminiService.java
+++ ai-service/src/main/java/com/microservice/project/ai_service/service/GeminiService.java
@@ -1,6 +1,5 @@
 package com.microservice.project.ai_service.service;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
