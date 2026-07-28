a33a78359ae4de09af691ad34124cd5e22e31c2d
Saransh Dhiman <saranshdhiman353@gmail.com>
2026-07-28 18:32:34 +0530

    adding the new function so only related user can see the own acivites

--- fitness-activity-microservice/src/main/java/com/microservice/project/fitness_microservice/repository/ActivityRepository.java
+++ fitness-activity-microservice/src/main/java/com/microservice/project/fitness_microservice/repository/ActivityRepository.java
@@ -1,10 +1,15 @@
 package com.microservice.project.fitness_microservice.repository;
 
 import com.microservice.project.fitness_microservice.entity.Activity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ActivityRepository extends MongoRepository<Activity, String> {
-
+    Page<Activity> findByUserid(String userid, Pageable pageable);
+    List<Activity> findByUserid(String userid);
 }
